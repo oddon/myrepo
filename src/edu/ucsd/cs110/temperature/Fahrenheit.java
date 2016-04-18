9c6630a4c2751a957366a1da0fba16e2b614501f
oddon <vo_don2@hotmail.com>
2016-04-18 12:47:25 -0700

    some message

--- src/edu/ucsd/cs110/temperature/Fahrenheit.java
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -1,47 +1,30 @@
 package edu.ucsd.cs110.temperature;
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.testng.annotations.Test;
 
-public class FahrenheitTest {
+/**
+ * Created by cs110xmatt on 3/11/16.
+ */
+public class Fahrenheit extends Temperature {
 
-    private float delta = 0.001f;
-    @Test
-    public void testFahrenheit(){
-        float value = 12.34f;
-        Fahrenheit temp = new Fahrenheit(value);
-        assertEquals(value, temp.getValue(), delta);
-    }
-    @Test
-    public void testFahrenheitToString(){
-        float value = 12.34f;
-        Fahrenheit temp = new Fahrenheit(value);
-        String string = temp.toString();
-        String beginning = "" + value;
-        String ending = " F";
-        // Verify the suffix of the formatted string
-        assertTrue(string.startsWith(beginning));
-        // Verify the prefix of the formatted string
-        assertTrue(string.endsWith(ending));
-        // Verify the middle of the formatted string
-        int endIndex = string.indexOf(ending);
-        // (Hint: what is the length of the middle of the string?)
-        assertTrue(string.substring(0, endIndex).equals(beginning));
-    }
-    @Test
-    public void testFahrenheitToFahrenheit()
+    public Fahrenheit(float t)
     {
-        Fahrenheit temp = new Fahrenheit(32);
-        Temperature convert = temp.toFahrenheit();
-        assertEquals(32, convert.getValue(), delta);
+        super(t);
     }
-    @Test
-    public void testFahrenheitToCelsius(){
-        Fahrenheit temp = new Fahrenheit(32);
-        Temperature convert = temp.toCelsius();
-        assertEquals(0, convert.getValue(), delta);
-        temp = new Fahrenheit(212);
-        convert = temp.toCelsius();
-        assertEquals(100, convert.getValue(), delta);
+
+    @java.lang.Override
+    public Temperature toCelsius() {
+        return new Celsius((super.value - 32) * 5 / 9);
+    }
+
+    @java.lang.Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit(super.value);
+    }
+
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return "" + super.value + " F";
     }
 }
+
